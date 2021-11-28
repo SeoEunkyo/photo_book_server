11df8e039c889f0718397ee393cc738e6479122e
Eunkyo <freehunterc@naver.com>
2021-11-28 22:16:26 +0900

    add func preSigned

--- routes/imageRouter.js
+++ routes/imageRouter.js
@@ -1,42 +1,95 @@
 const {Router} = require("express");
+const { v4: uuid } = require('uuid')
+const mime = require('mime-types');
+
 const imageRouter = Router();
 const Image = require('../models/Images');
 const {upload} = require('../middleware/imageUpload');
 const fs = require('fs');
 const {promisify} = require('util');
 const mongoose = require('mongoose');
+const {s3, getSignedUrl} = require('../aws')
 
-const fileUnlink = promisify(fs.unlink);
+// const fileUnlink = promisify(fs.unlink);
 
-imageRouter.post('/', upload.array("image",5), async (req, res) => {
+imageRouter.post("/presigned", async(req,res)=>{
+    try{
+        if(!req.user) throw new Error("권하니 없습니다.");
+        const {contentTypes} = req.body;
+        if(!Array.isArray(contentTypes)) throw new Error("정상적인 파일이 아닙니다");
+
+        const presignedData = await Promise.all(  contentTypes.map(async (contentType)=>{
+            const imageKey = `${uuid()}.${mime.extension(contentType)}`;
+            const key =`raw/${imageKey}`;
+            const presigned = await getSignedUrl({key});
+            return {imageKey, presigned}
+        }))
+
+
+        res.json(presignedData);
+
+    }catch(err){
+
+    }
+
+})
+
+imageRouter.post('/', upload.array("image", 5), async (req, res) => {
     // 유저 정보 확인 , public 유뮤확인
 
     try {
-        if(!req.user) throw new Error("사용자 권한이 없습니다");
-        const images = await Promise.all(
-            req.files.map(async file => {
-                const image = await new Image({
-                    user: {
-                        _id: req.user.id,
-                        name: req.user.name,
-                        username: req.user.username,
-                    },
-                    public: req.body.public,
-                    key: file.filename,
-                    originalFileName: file.originalname
-                }).save();
+        if (!req.user) throw new Error("사용자 권한이 없습니다");
+        const { images, public } = req.body;
 
-                return image
-            })
-        ) 
-        
-        res.json(images);
+        const imageDocs = await Promise.all(images.map((image) => new Image({
+            user: {
+                _id: req.user.id,
+                name: req.user.name,
+                username: req.user.username,
+            },
+            public,
+            key: image.imagekey,
+            originalFileName: image.originalname
+        }).save()
+        ))
+
+        res.json(imageDocs);
     } catch (error) {
         console.error(error);
         res.status(400).json({ message: error.message });
     }
 
 })
+
+// imageRouter.post('/', upload.array("image",5), async (req, res) => {
+//     // 유저 정보 확인 , public 유뮤확인
+
+//     try {
+//         if(!req.user) throw new Error("사용자 권한이 없습니다");
+//         const images = await Promise.all(
+//             req.files.map(async file => {
+//                 const image = await new Image({
+//                     user: {
+//                         _id: req .user.id,
+//                         name: req.user.name,
+//                         username: req.user.username,
+//                     },
+//                     public: req.body.public,
+//                     key: file.key,
+//                     originalFileName: file.originalname
+//                 }).save();
+
+//                 return image
+//             })
+//         ) 
+        
+//         res.json(images);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(400).json({ message: error.message });
+//     }
+
+// })
 imageRouter.get('/', async (req, res) => {
     // public 이미지 제공
     const images = await Image.find();
@@ -51,7 +104,10 @@ imageRouter.delete('/:imageId' , async (req, res) =>{
 
         const imagefile = await Image.findOneAndDelete({_id: req.params.imageId});
         if(!imagefile) return res.json({message : "요청하신 이미지는 이미 삭제가 되었습니다."});
-        await fileUnlink(`./uploads/${imagefile.key}`);
+        // await fileUnlink(`./uploads/${imagefile.key}`);
+        s3.deleteObject({Bucket:'image-upload-tutorial2', key:`raw/${image.key}`}, (err,data)=> {
+            if(error) throw error;
+        })
         res.json({message : "삭제를 완료하였습니다", imagefile});
 
     } catch (error) {
